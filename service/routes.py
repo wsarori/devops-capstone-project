21a7331039dc263f5a4ca4de24d5561a53570976
Waqas Qasem <71858112+wsarori@users.noreply.github.com>
2024-08-13 07:58:10 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,7 +57,12 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
-
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
+    ######################################################################
+    # LIST ALL ACCOUNTS
+    ######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -65,19 +70,45 @@ def list_accounts():
     This endpoint will list all Accounts
     """
     app.logger.info("Request to list Accounts")
+
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
+
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+# ... place you code here to LIST accounts ...
+    ######################################################################
+    # LIST ALL ACCOUNTS
+    ######################################################################
+    ##########################################
+# READ AN ACCOUNT
+######################################################################
 
+    ######################################################################
+    # READ AN ACCOUNT
+    ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
     app.logger.info("Request to read an Account with id: %s", account_id)
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
     return jsonify(account.serialize()), status.HTTP_200_OK
 
+
+######################################################################
+# UPDATE AN EXISTING ACCOUNT
+######################################################################
+
+    ######################################################################
+    # UPDATE AN EXISTING ACCOUNT
+    ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
